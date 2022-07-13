d6d7e753aef8ef4ae833200e8cf3fde7a431d75e
Murilo Olegini <muriloolegini@outlook.com>
2022-07-13 16:15:17 -0300

    Initial commit

---
+++ geradordesenhas.py
@@ -0,0 +1,7 @@
+import random, string
+
+tamanho = int(input('Digite o tamanho de senha que você deseja: '))
+chars = string.ascii_letters + string.digits + 'ç!@#$%&*()-=+*/\,.?'
+rnd = random.SystemRandom()
+
+print(''.join(rnd.choice(chars) for i in range(tamanho)))
